b4aeae0d3181eae1d0951d17bb837233b3d7588f
tylergeorges <tmg320v@gmail.com>
2022-08-03 13:18:35 -0500

    first init

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum {
+
+	
+	
+	
+	public static int getSum(int numOne, int numTwo) {
+		
+		
+		return numOne + numTwo;
+		
+	}
+	
+
+
+}
---
+++ src/test/java/TwoSumTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwoSumTest {
+
+	@Test
+	public void testTwoSum() {
+		
+		assertEquals(5, TwoSum.getSum(3,2));
+	}
+
+}
